dccf76f6d23c48b6da0ce33d5ff77e8dffaad1e1
La-Pulga <camille.labaronne@hotmail.fr>
2016-03-24 21:00:51 +0100

    Initial import

---
+++ Portfolio_Projects.php
@@ -0,0 +1,52 @@
+<?php include("Portfolio_Header.html") ; ?>
+
+<link rel="stylesheet" href="Portfolio.css">
+
+
+
+<h2> This web site </h2> </br>
+<h4> Main tasks and goals </h4>
+<ol>
+	<li> Use the .jpeg file from <a href=" https://openclassrooms.com/courses/apprenez-a-creer-votre-site-web-avec-html5-et-css3" > Openclassrooms </a>  to re-create the Portfolio template using HTML5 and CSS3. </li>
+	<li> Use the creation of this Portfolio as a final exercise after completing the HTML5 and CSS3 course on <a href="https://www.coursera.org/learn/introcss/">Coursera </a> with the University of Michigan. </li>
+	<li> Create a static website, then making it dynamic.</li>
+</ol>
+
+<h4> Problems encountered </h4>
+<ol>
+	<li> Understanding the general architecture of a website, including the part of the language server (in this case Php) and the database. </li>
+	<li> Figuring how to re-use some parts of the main page onto other pages. </li>
+</ol>	
+
+<h4> Solutions </h4>
+<ol>
+	<li> Watching tutorials explaining how and why to use a local server, reading articles about web architecture, completing courses on Coursera. </li>
+	<li> Learning server languages such as Php and Ruby on <a href="https://www.codecademy.com/Cami-Camila"> Code Academy. </a>	</li>
+	<li> Creating .html templates with each re-usable part, and include those pages to the .php files with a simple line of code. </li>
+</ol>
+
+<h2> Pure CSS : CSS Zen Garden </h2> </br>
+<h4> Main tasks and goals</h4>
+	<ol>
+		<li> Designing a webpage with a given HTML file. </li>
+		<li> Push the abilities of CSS3 further, even if some features should have been made with JS. </li>
+	</ol>
+<h4> Problems encountered </h4>
+	<ol> 
+		<li> Designing a page, creating a user-friendly design with a nice rendering. </li>	
+		<li> Understanding the box positioning with CSS3 </li>
+		<li> Creating the menu and displaying the contents in a position relative to the main page and not to the corresponding h3 titles </li>	
+		<li> Make menu content display stop on click, make footer stick at the bottom of the page. </li>
+	</ol>		
+<h4> Solutions </h4>
+	<ol>
+		<li> Using the color wheel and the complementary colors, using wave.webaim.org to check accessibility. </li>
+		<li> Watching tutotials, reading articles, putting the layout of the webpage down on paper, working with Code Pen to get immediate results. </li>
+		<li> Setting all boxes to position relative except the footer, set to position absolute. </li>
+	</ol>	
+
+<?php include 'Portfolio_Footer.html' ;
+?>	
+
+
+	
